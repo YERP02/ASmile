e216d7f0a0d224d68afbb2876245dc6925fa722a
YERP02 <yahiremanuelramirezpulido@gmail.com>
2026-03-17 03:31:24 -0600

    Create ASmile web

---
+++ web/web/src/main/java/com/asmile/web/AsmileWebApplication.java
@@ -0,0 +1,15 @@
+package com.asmile.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsmileWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsmileWebApplication.class, args);
+
+		System.out.println("hola");
+	}
+	
+}
---
+++ web/web/src/test/java/com/asmile/web/AsmileWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asmile.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsmileWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
